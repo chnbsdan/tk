119351df7e3e4b91dc6c45654d794236cbc5c3e2
chnbsdan <chnbsdan@gmail.com>
2026-06-10 11:00:48 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://tk.hangdn.com',
-        changeOrigin: true,
-      },
-      '/random': {
-        target: 'https://tk.hangdn.com',
-        changeOrigin: true,
-      },
-      '/json': {
-        target: 'https://tk.hangdn.com',
-        changeOrigin: true,
-      },
-      '/list': {
-        target: 'https://tk.hangdn.com',
-        changeOrigin: true,
-      },
-      '/stats': {
-        target: 'https://tk.hangdn.com',
-        changeOrigin: true,
-      },
-    },
-  },
 })
